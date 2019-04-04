26c4db24c1e33bd49a319720016f6f6c395d3aae
Aaron Chu <aaronchu357@gmail.com>
2019-04-04 21:10:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,16 @@
 def line(arr)
+  if arr.length == 0
+    puts "The line is currently empty."
+  else
+    i = 0
+    output = "The line is currently: "
+    while i < arr.length  
+      output += "#{i + 1}. #{arr[i]} "
+    end
+  end
+end
 
+  
 end
 
 def take_a_number(arr, str)
