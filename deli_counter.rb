e6e6c1768ee390098fa29e170a2fc7405f6e9a1d
Aaron Chu <aaronchu357@gmail.com>
2019-04-04 21:10:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,13 @@ def line(arr)
   else
     i = 0
     output = "The line is currently: "
-    while i < arr.length  
+    while i < arr.length
       output += "#{i + 1}. #{arr[i]} "
     end
   end
 end
 
-  
+
 end
 
 def take_a_number(arr, str)
