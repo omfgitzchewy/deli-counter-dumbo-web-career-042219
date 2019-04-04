d24c3c271f3c5e08ab02bb93d4d4f5595983ef1b
Aaron Chu <aaronchu357@gmail.com>
2019-04-04 21:28:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,6 @@ def now_serving(arr)
   if arr.length == 0
     puts "There is nobody waiting to be served!"
   end
-  arr.shift()
-
+  curr_per = arr.shift()
+  puts "Currently serving #{curr_per}."
 end
