ce53863891975f190e2fa2d53e78425c0a97afb1
Aaron Chu <aaronchu357@gmail.com>
2019-04-04 21:15:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(arr)
   if arr.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
   i = 0
   output = "The line is currently: "
@@ -8,7 +8,7 @@ def line(arr)
     output += "#{i + 1}. #{arr[i]} "
     i += 1
   end
-  return output
+  puts output
   end
 end
 
